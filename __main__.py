d57a613f3c33e094ea7bf9196f147c93f2678be0
mervo <9879403+mervo@users.noreply.github.com>
2021-05-18 09:45:23 +0800

    added runnable main

---
+++ __main__.py
@@ -0,0 +1,12 @@
+# python -m perception_tools from parent folder
+
+from .perception_convert import *
+
+perception_folder = '/home/user/.config/unity3d/DefaultCompany/tutorial/a37a249f-940d-47fc-947b-143f325114e2'
+
+if __name__ == '__main__':
+    print(perception_folder)
+    coco_dataset = convert_perception(perception_folder)
+
+    print(coco_dataset)
+
